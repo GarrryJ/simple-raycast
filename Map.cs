70e76787aa6badc103334a2a42c1155af7e9fd5a
ubersenpai <davydovsiu@gmail.com>
2021-01-27 01:51:22 +1000

    Added Map class

---
+++ Map.cs
@@ -0,0 +1,25 @@
+namespace simple_raycast {
+    public class Map {
+        private int mapHeight {get; set;}
+        private int mapWidth {get; set;}
+        private int[] map {get; set;}
+
+        public Map(int h, int w, int[] map) {
+            this.map = map;
+            this.mapHeight = h;
+            this.mapWidth = w;
+        }
+
+        public int getMapHeigth() {
+            return this.mapHeight;
+        }
+
+        public int getMapWidth() {
+            return this.mapWidth;
+        }
+
+        public int[] getMap() {
+            return this.map;
+        }
+    }
+}
