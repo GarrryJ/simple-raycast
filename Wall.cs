879dbe86d90c4bc0b4c862e1103c6cc5cda8112b
ubersenpai <davydovsiu@gmail.com>
2021-01-28 02:36:49 +1000

    Map updated (lol check it)

--- Wall.cs
+++ Wall.cs
@@ -4,14 +4,24 @@ using System;
 namespace simple_raycast {
     public static class Wall {
         public static Color getColorById(int id, int darkness) {
-            if (id == 4)
+            if (id == 4) 
                 id = new Random().Next(3) + 1;
+            Color color;
             switch (id) {
-                case 1: return new Color(255 - darkness, 0, 0);
-                case 2: return new Color(0, 255 - darkness, 0);
-                case 3: return new Color(0, 0, 255 - darkness);
-                default: return new Color(0, 0, 0);
+                case 1:
+                    color = new Color(255 - darkness, 0, 0);
+                    break;
+                case 2: 
+                    color = new Color(0, 255 - darkness, 0);
+                    break;
+                case 3: 
+                    color = new Color(0, 0, 255 - darkness);
+                    break;
+                default: 
+                    color = new Color(0, 0, 0);
+                    break;
             }
+            return color;
         }
     }
 }
